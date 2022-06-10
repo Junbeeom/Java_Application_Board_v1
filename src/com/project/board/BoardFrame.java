9a3fc54bd83f9ec98fd056741832e6aae3eb03e3
Junbeeom <wns_p@naver.com>
2022-06-10 23:42:35 +0900

    init

---
+++ src/com/project/board/BoardFrame.java
@@ -0,0 +1,16 @@
+package com.project.board;
+
+import java.util.Scanner;
+
+public class BoardFrame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Board board = new Board();
+
+        boolean flag = true;
+
+        while (flag) {
+        }
+    }
+}
